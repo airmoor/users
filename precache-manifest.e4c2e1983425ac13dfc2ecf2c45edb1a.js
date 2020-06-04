c3321f3eaf80b6bfb5ae2ae0d6ee60c1c43c1422
Anastasia <nastia.schatskaya@gmail.com>
2020-06-05 00:07:41 +0300

    Updates

---
+++ precache-manifest.e4c2e1983425ac13dfc2ecf2c45edb1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556fd4d4d02941ee3a2fe342ee776543",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "61a0c84e04847d0d7c5b",
+    "url": "/users/static/css/main.fbf05918.chunk.css"
+  },
+  {
+    "revision": "14fa88002894ff25e7c8",
+    "url": "/users/static/js/2.00650d94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/users/static/js/2.00650d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a0c84e04847d0d7c5b",
+    "url": "/users/static/js/main.58784daf.chunk.js"
+  },
+  {
+    "revision": "ef897bbfb1f7d0f22006",
+    "url": "/users/static/js/runtime-main.8b58aa93.js"
+  }
+]);
